c9e7a96632803dbe1c691ddb65b8d188e06516bc
Derek Bredensteiner <derekbreden@gmail.com>
2024-07-06 21:01:04 +0000

    Track what’s expanded and collapsed and allow re-collapsing

--- client/renderComments.js
+++ client/renderComments.js
@@ -41,7 +41,10 @@ const renderComments = (comments) => {
   // Identify the root comments (threads) to be displayed
   const $root_comments = comments
     .filter((c) => !c.parent_comment_id)
-    .map((c) => c.$comment);
+    .map((c) => {
+      c.$comment.comment_id = c.comment_id;
+      return c.$comment;
+    });
 
   // Collapse all the intermediates in each thread, if present
   $root_comments.forEach(($root_comment) => {
@@ -49,26 +52,77 @@ const renderComments = (comments) => {
     if ($child_comments.length > 1) {
       const $last_comment = $child_comments.pop();
       const $original_parent = $last_comment.parentElement;
-      $root_comment.appendChild($last_comment);
-      $child_comments.forEach(($child_comment) => {
-        $child_comment.style.display = "none";
+
+      // Collapse button
+      const $collapse_button = $(
+        `
+        expand-wrapper[collapse]
+          button[expand-left]
+          p $1
+          button[expand-right]
+        `,
+        [`Hide ${$child_comments.length} comments`],
+      );
+      $collapse_button.on("click", () => {
+        // Track what is collapsed
+        const index = state.expanded_comment_ids.indexOf(
+          $root_comment.comment_id,
+        );
+        if (index !== -1) {
+          state.expanded_comment_ids.splice(index, 1);
+          localStorage.setItem(
+            "expanded_comment_ids",
+            JSON.stringify(state.expanded_comment_ids),
+          );
+        }
+
+        // Add and remove the buttons
+        $root_comment.$(":scope > reply-wrapper").after($expand_button);
+        $collapse_button.remove();
+
+        // Move last child to bottom of root
+        $root_comment.appendChild($last_comment);
+
+        // Show intermediates
+        $child_comments.forEach(($child_comment) => {
+          $child_comment.style.display = "none";
+        });
       });
+
+      // Expand button
       const $expand_button = $(
         `
-        expand-wrapper
+        expand-wrapper[expand]
           button[expand-up]
           p $1
           button[expand-down]
         `,
-        [`Show ${$child_comments.length} hidden comments`]
+        [`Show ${$child_comments.length} hidden comments`],
       );
       $expand_button.on("click", ($event) => {
+        // Track what is expanded
+
+        const index = state.expanded_comment_ids.indexOf(
+          $root_comment.comment_id,
+        );
+        if (index === -1) {
+          state.expanded_comment_ids.push($root_comment.comment_id);
+          localStorage.setItem(
+            "expanded_comment_ids",
+            JSON.stringify(state.expanded_comment_ids),
+          );
+        }
+
+        // Track for scroll position
         const original_rect = $last_comment.getBoundingClientRect();
+
+        // Add and remove the buttons
         $expand_button.remove();
+        $root_comment.$(":scope > reply-wrapper").after($collapse_button);
 
         // Bring all the collapsed nodes back and flash them
         $child_comments.forEach(($child_comment) => {
-          $child_comment.style.display = "";
+          $child_comment.style.display = "flex";
           $child_comment.setAttribute("flash-focus", "");
         });
 
@@ -77,14 +131,18 @@ const renderComments = (comments) => {
           $original_parent.appendChild($last_comment);
         }, 500);
 
-        // When they click down, keep focus on the last comment
+        // When they click down, keep scroll on the last comment
         if ($event.target.hasAttribute("expand-down")) {
           const final_rect = $last_comment.getBoundingClientRect();
           const body = document.scrollingElement || document.documentElement;
           body.scrollTop = body.scrollTop + (final_rect.y - original_rect.y);
         }
       });
-      $last_comment.before($expand_button);
+      if (state.expanded_comment_ids.includes($root_comment.comment_id)) {
+        $expand_button.click();
+      } else {
+        $collapse_button.click();
+      }
     }
   });
 
