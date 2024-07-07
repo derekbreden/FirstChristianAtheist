efd694386e811920b3c2c5e4f583b992a3f45e34
Derek Bredensteiner <derekbreden@gmail.com>
2024-07-07 04:23:25 +0000

    Fix path parsing on popstate as well

--- client/path.js
+++ client/path.js
@@ -1,22 +1,24 @@
 // Read URL into path
-const paths = window.location.pathname.split("/").filter((x) => x);
-if (paths[0] === "reset") {
-  state.reset_token_uuid = paths[1] || "";
-} else if (paths[0] === "article" && paths[1]) {
-  state.path = "/" + paths[0] + "/" + paths[1];
-} else if (paths[0] === "comment" && paths[1]) {
-  state.path = "/" + paths[0] + "/" + paths[1];
-} else if (paths[0]) {
-  state.path = "/" + paths[0];
+const parsePath = () => {
+  let new_path = "/";
+  const new_paths = window.location.pathname.split("/").filter((x) => x);
+  if (new_paths[0] === "reset") {
+    state.reset_token_uuid = new_paths[1] || "";
+  } else if (new_paths[0] === "article" && new_paths[1]) {
+    new_path = "/" + new_paths[0] + "/" + new_paths[1];
+  } else if (new_paths[0] === "comment" && new_paths[1]) {
+    new_path = "/" + new_paths[0] + "/" + new_paths[1];
+  } else if (new_paths[0]) {
+    new_path = "/" + new_paths[0];
+  }
+  return new_path;
 }
+state.path = parsePath();
+
 
 // Update page contents when the user hits the back button
 window.addEventListener("popstate", () => {
-  const new_paths = window.location.pathname.split("/").filter((x) => x);
-  let new_path = "/";
-  if (new_paths[0]) {
-    new_path = "/" + new_paths[0];
-  }
+  const new_path = parsePath();
   if (state.path !== new_path) {
     state.path = new_path;
     loadingPage();
